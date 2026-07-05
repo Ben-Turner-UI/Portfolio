b296a085c70361b21c52fcb70385f2edb22986bc
Ben-Turner-UI <32572698+Ben-Turner-UI@users.noreply.github.com>
2026-07-05 20:43:09 +0100

    n

--- count.ps1
+++
@@ -1,5 +0,0 @@
-param($file)
-$t = Get-Content $file -Raw
-$b = [regex]::Match($t, '(?s)<main>.*</main>').Value
-$s = [regex]::Replace($b, '<[^>]+>', ' ')
-($s -split '\s+' | Where-Object { $_ }).Count
